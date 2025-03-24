0b40765fa5157e5f861cd97f6b6d726833ad41de
Elijah Peter <133159566+elijah571@users.noreply.github.com>
2025-03-24 13:16:28 +0100

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"], 
+        origin: ["http://localhost:5173", "https://chat-app-32sc.onrender.com"], 
         methods: ["POST", "GET", "DELETE", "PUT"],
     },
 });
