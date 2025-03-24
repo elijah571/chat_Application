841a78028f0544ca8f97480cf7c8b429172eda97
Elijah Peter <133159566+elijah571@users.noreply.github.com>
2025-03-24 13:26:38 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,19 @@ import { messageRoute } from './routers/message.route.js';
 import { authRoute } from './routers/auth.route.js';
 import { app, server } from './socket/socket.js';
 import express from 'express'
-import path from 'path';
 dotenv.config();
-const PORT = process.env.PORT|| 5000
-const __dirname = path.resolve()
+
+const PORT = process.env.PORT || 5000
 
 connectDb()
 app.use(cookieParser());
-// apis
+
+// APIs
 app.use('/api/user', userRoute)
 app.use('/api/auth', authRoute)
 app.use('/api/messages', messageRoute)
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
+
 server.listen(PORT, () => {
     console.log('server running on port:', PORT)
 })
