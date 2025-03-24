f1b484d488f196a35c9f4470983f5a54bdad3cb7
elijah571 <elijahfx43@gmail.com>
2025-03-24 11:11:20 +0100

    Fix: Updated Vite proxy for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api/': {
-        target: 'http://localhost:5000', 
+        target: "https://chat-app-4j3t.onrender.com",
         changeOrigin: true,
         secure: false,
       },
