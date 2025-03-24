0b6efeaaf35486752aee5c319450e42777ca7e91
elijah571 <elijahfx43@gmail.com>
2025-03-24 10:41:57 +0100

    reinstallation of vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     proxy: {
-      "/api/": {
-        target: "http://localhost:5000",
+      '/api/': {
+        target: 'http://localhost:5000', 
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
-      
     },
   },
 });
